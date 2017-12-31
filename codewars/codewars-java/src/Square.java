d208a4b278688b14d9c245236a9493da50c5c896
Aren <aren.tahmasian.745@my.csun.edu>
2017-12-31 14:28:11 -0800

    CW-Java Sqaure

---
+++ codewars/codewars-java/src/Square.java
@@ -0,0 +1,32 @@
+/*My Solution*/
+public class Square {
+    public static void main(String[] args){
+    }
+
+    public static boolean isSquare(int n) {
+        if(Math.sqrt(n) % (int)Math.sqrt(n) == 0 )
+            return true;
+        else
+            return false;
+    }
+}
+
+/*Other Solutions*/
+/*
+import static java.lang.Math.*;
+public class Square {
+    public static boolean isSquare(int n) {
+        return Math.sqrt(n) % 1 == 0;
+    }
+}
+*/
+
+/*
+public class Square {
+    public static boolean isSquare(int n) {
+        long s = Math.round(Math.sqrt(n));
+        return s * s == n;
+    }
+}
+*/
+
