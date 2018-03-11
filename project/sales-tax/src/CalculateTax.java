018f7b924658081622e0d2255fe4a9375eeba2c2
Aren <aren.tahmasian.745@my.csun.edu>
2018-03-10 19:57:10 -0800

    Sales Tax - Defined Problem and Goal

---
+++ project/sales-tax/src/CalculateTax.java
@@ -0,0 +1,26 @@
+public class CalculateTax {
+
+    // GOAL
+    // ========================================
+    // Object-oriented class that performs the calculation of tax (sales & import) and displays an output
+    // of the items purchased with their respective price, the total amount of sales tax paid, and the total cost
+
+    // SPECIFICATIONS
+    // ========================================
+    // Code must be configurable, i.e. the user must be able to change tax rates, change currencies, add new taxes, etc
+    // Round up to the nearest .05 cents for tax calculations
+    // Include all source documents
+
+    // CLASSES
+    // ========================================
+    // CalculateTax class to handle object creation
+    // ConfigTaxSettings class? interface? to handle configurable options
+    // BigDecimal class to handle tax calculations
+    // NumberFormat class for displaying, or maybe right your own class
+
+    // UNIT TESTING
+    // ========================================
+    // Test methods with various types of input to ensure functionality, reliability, Maintainability, Usability
+
+
+}
---
+++ project/sales-tax/src/TestEnvironment.java
@@ -0,0 +1,4 @@
+public class TestEnvironment {
+    public static void main(String[] args){
+    }
+}
