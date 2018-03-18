6320bf4f9b6fb0116c60d135abbf601e5350e57c
Aren <aren.tahmasian.745@my.csun.edu>
2018-03-17 20:24:55 -0700

    JSON Parser - Added Apache Commons IO Library

--- project/json-parser/src/JsonParser.java
+++ project/json-parser/src/JsonParser.java
@@ -1,8 +1,17 @@
 import org.json.*;
+import java.io.File;
+import org.apache.commons.io.FileUtils;
+
 
 public class JsonParser {
 
     public static void main(String[] args){
+
+        JSONObject jsonObject = new JSONObject("{\"name\": \"John\"}");
+
+        String type = jsonObject.getString("name");
+
+        System.out.println(type);
 
     }
 
