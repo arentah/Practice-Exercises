58ed376e657948d9e089019f860e0acd32eafbe0
Aren <aren.tahmasian.745@my.csun.edu>
2018-03-17 20:40:56 -0700

    JSON Parser - Sucessfully Tested Parsing "sample"

--- project/json-parser/src/JsonParser.java
+++ project/json-parser/src/JsonParser.java
@@ -6,13 +6,23 @@ import org.apache.commons.io.FileUtils;
 public class JsonParser {
 
     public static void main(String[] args){
-
-        JSONObject jsonObject = new JSONObject("{\"name\": \"John\"}");
-
-        String type = jsonObject.getString("name");
-
-        System.out.println(type);
-
+        try {
+            readJSON();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
+    private static void readJSON() throws Exception {
+        File file = new File("./json/sample.json");
+        String content = FileUtils.readFileToString(file, "utf-8");
+        JSONObject sample = new JSONObject(content);
+
+        JSONArray jsonArray = sample.getJSONArray("features");
+
+        JSONObject jsonObject = (JSONObject) jsonArray.get(0);
+        System.out.println(jsonObject.getString("type"));
+
+        //https://www.codevoila.com/post/65/java-json-tutorial-and-example-json-java-orgjson
+    }
 }
