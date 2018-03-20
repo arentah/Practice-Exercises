58ead9d85526abd8a635da7e9a692413e8422e9a
Aren <aren.tahmasian.745@my.csun.edu>
2018-03-19 22:22:22 -0700

    JSON Parser - Added Nested JSON Parsing Ability

--- project/json-parser/src/JsonParser.java
+++ project/json-parser/src/JsonParser.java
@@ -1,28 +1,86 @@
 import org.json.*;
 import java.io.File;
-import org.apache.commons.io.FileUtils;
+import java.util.ArrayList;
 
+import org.apache.commons.io.FileUtils;
 
 public class JsonParser {
 
     public static void main(String[] args){
+        JsonParser parser = new JsonParser();
         try {
-            readJSON();
+            parser.readJSON();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private static void readJSON() throws Exception {
+    private void readJSON() throws Exception {
         File file = new File("./json/sample.json");
         String content = FileUtils.readFileToString(file, "utf-8");
         JSONObject sample = new JSONObject(content);
-
         JSONArray jsonArray = sample.getJSONArray("features");
 
-        JSONObject jsonObject = (JSONObject) jsonArray.get(0);
-        System.out.println(jsonObject.getString("type"));
+        String type;
 
+        ArrayList<Double> res = new ArrayList<>();
+        for(int i = 0; i < jsonArray.length(); i++){
+            JSONObject jsonObject = (JSONObject) jsonArray.get(i);
+            try{
+                type = jsonObject.getString("type");
+                JSONArray jsonArray1 = jsonObject.getJSONObject("geometry").getJSONArray("dimensions");
+                res.addAll(printDouble(jsonArray1));
+
+                Double[] tmp = arrayListToDoubleArray(printDouble(jsonArray1));
+                System.out.println("----------------");
+                System.out.println(type);
+                for(Double d : tmp)
+                    System.out.print(d+" ");
+                System.out.println("\n****************\n");
+            }catch(Exception e){
+                System.out.println("failing at: " + i +" error: "+ e);
+            }
+        }
+        System.out.println("-----total------");
+        for(Double d : res)
+            System.out.print(d+" ");
+        System.out.println("\n****************");
         //https://www.codevoila.com/post/65/java-json-tutorial-and-example-json-java-orgjson
     }
+
+
+    private static ArrayList<Double> printDouble(JSONArray array) throws IllegalArgumentException{
+        ArrayList<Double> res = new ArrayList<>();
+        for(Object obj : array){
+            if(obj instanceof Double){
+                res.add((Double) obj);
+            }else if(obj instanceof JSONArray){
+                res.addAll(printDouble((JSONArray)obj));
+            }else{
+                throw new IllegalArgumentException("Invalid input... Expecting array of Double or arrays of arrays");
+            }
+        }
+        return res;
+    }
+
+    private Double[] arrayListToDoubleArray(ArrayList<Double> array){
+        Double[] result = new Double[array.size()];
+        for(int i = 0; i < array.size(); i++){
+            result[i] = array.get(i);
+        }
+        return result;
+    }
+
+    private double[] jsonToDoubleArray(JSONArray jsonArray){
+        double[] result = new double[jsonArray.length()];
+        for(int i = 0; i < jsonArray.length(); i++){
+            result[i] = jsonArray.getDouble(i);
+        }
+        return result;
+    }
 }
+
+
+
+
+
