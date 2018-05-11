d3ab60085bb920521fac3af0cd8efb6b5445a829
Aren <aren.tahmasian.745@my.csun.edu>
2018-05-10 22:00:45 -0700

    CC-Java Question 2.3

--- crackingcode/crackingcode-java/src/Ch2_Question2.java
+++ crackingcode/crackingcode-java/src/Ch2_Question2.java
@@ -2,14 +2,12 @@ import java.util.LinkedList;
 
 public class Ch2_Question2 {
     public static void main(String[] args){
-
         LinkedList<String> list = new LinkedList<>();
         list.add("hello");list.add("world");list.add("what");list.add("a");list.add("beautiful");list.add("day!");
         System.out.println(kthToLast(list,2));
     }
 
     public static String kthToLast(LinkedList<String> list, int k){
-
         int size = list.size() - 1;
         if(k >= size)
             return "Outside boundaries of linked list...";
---
+++ crackingcode/crackingcode-java/src/Ch2_Question3.java
@@ -0,0 +1,15 @@
+public class Ch2_Question3 {
+
+    //Algorithm to delete node in middle of linked list
+    //Given: access to the middle node
+
+    //if(node == head || node == tail || node == null)
+    //    return false;
+
+    //Node prev = node.prev;
+    //Node next = node.next;
+    //prev.next = next;
+    //next.prev = prev;
+    //return true;
+
+}
